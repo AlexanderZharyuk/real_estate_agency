d7632d677b06454c87bedb7abe1246f43de5a3e7
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-20 17:30:06 +0300

    Add admin register decorator

--- property/admin.py
+++ property/admin.py
@@ -10,6 +10,7 @@ class OwnersInline(admin.TabularInline):
     raw_id_fields = ['owner']
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town']
     readonly_fields = ['created_at']
@@ -24,14 +25,11 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnersInline]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['author', 'flat']
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
