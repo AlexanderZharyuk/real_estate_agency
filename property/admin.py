c22d82dbc98222885f0692e8b40584e32981b181
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-20 18:07:44 +0300

    Database normalizations

--- property/admin.py
+++ property/admin.py
@@ -15,8 +15,7 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town']
     readonly_fields = ['created_at']
 
-    list_display = ['address', 'price', 'new_building', 'construction_year',
-                    'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
